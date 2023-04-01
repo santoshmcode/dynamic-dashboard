7c58a30705db9ccad4b90267559e14fc8de19337
Santosh Mane <santoshmane619@gmail.com>
2023-04-01 18:11:43 +0530

    server: added route for individual dashboard

--- server/src/routers/dashboard.router.ts
+++ server/src/routers/dashboard.router.ts
@@ -15,4 +15,18 @@ router.get('/', (req: Request, res: Response) => {
     });
 });
 
+router.get('/:id', (req: Request, res: Response) => {
+    console.log("Inside GET /:id")
+    fs.readFile(__dirname + '/../db/dashboard.json', 'utf8', (err, data: any) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Internal Server Error');
+        }
+        const id: any = req.params.id
+        const jsonData = JSON.parse(data);
+        const singleDashBoardData = jsonData[id]
+        return res.send(singleDashBoardData);
+    });
+});
+
 export default router;
