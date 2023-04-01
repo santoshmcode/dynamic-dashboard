4472118dbe6aba04eb7136f95ab09d3543d1c026
Santosh Mane <santoshmane619@gmail.com>
2023-04-01 17:43:47 +0530

    server: install cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors';
 import dashboardRouter from "./routers/dashboard.router"
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', dashboardRouter);
 
 app.get("/", (req, res) => {
