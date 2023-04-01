0c047b0d69daf398791c85baa38a6f9a1532d84f
Santosh Mane <santoshmane619@gmail.com>
2023-04-01 23:40:46 +0530

    server: added types

--- server/src/routers/dashboard.router.ts
+++ server/src/routers/dashboard.router.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response } from 'express';
 import * as fs from 'fs';
+import { Dashboard, DashboardData, Widget } from '../types/types';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    fs.readFile(__dirname + '/../db/allDashboardsData.json', 'utf8', (err, data) => {
+    fs.readFile(__dirname + '/../db/allDashboardsData.json', 'utf8', (err, data: string) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
         }
 
-        const jsonData = JSON.parse(data);
+        const jsonData: Dashboard[] = JSON.parse(data);
         return res.send(jsonData);
     });
 });
@@ -18,26 +19,26 @@ router.get('/', (req: Request, res: Response) => {
 let count = 0
 router.get('/:id', (req: Request, res: Response) => {
     console.log("Inside GET /:id", count++)
-    fs.readFile(__dirname + '/../db/dashboardData.json', 'utf8', (err, data: any) => {
+    fs.readFile(__dirname + '/../db/dashboardData.json', 'utf8', (err, data: string) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
         }
         const id: string = req.params.id
-        const jsonData = JSON.parse(data);
+        const jsonData: DashboardData = JSON.parse(data);
         const singleDashBoardData = jsonData[id]
         return res.send(singleDashBoardData);
     });
 });
 
 router.get('/widget/:id', (req: Request, res: Response) => {
-    fs.readFile(__dirname + '/../db/widgetsData.json', 'utf8', (err, data: any) => {
+    fs.readFile(__dirname + '/../db/widgetsData.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
         }
         const { id } = req.params
-        const jsonData = JSON.parse(data);
+        const jsonData: Widget = JSON.parse(data);
         return res.send(jsonData[id]);
     });
 });
---
+++ server/src/types/types.d.ts
@@ -0,0 +1,116 @@
+interface Dashboard {
+    _id: number;
+    identifier: string;
+}
+
+interface DashboardData {
+    [dashboardId: string]: {
+        _id: string;
+    }[];
+}
+
+interface Card {
+    type: {
+        name: string;
+        subtype: string;
+    };
+    _id: string;
+    position: number;
+    title: string;
+    value: string;
+}
+
+interface List {
+    type: {
+        name: string;
+        subtype: string;
+    };
+    _id: string;
+    position: number;
+    title: string;
+    items: string[];
+}
+
+interface PieGraph {
+    type: {
+        name: string;
+        subtype: string;
+    };
+    _id: string;
+    position: number;
+    title: string;
+    data: {
+        label: string;
+        value: number;
+    }[];
+}
+
+interface LineGraph {
+    type: {
+        name: string;
+        subtype: string;
+    };
+    _id: string;
+    position: number;
+    title: string;
+    data: {
+        x: number;
+        y: number;
+    }[];
+    xAxisLabel: string;
+    yAxisLabel: string;
+}
+
+interface BarGraph {
+    type: {
+        name: string;
+        subtype: string;
+    };
+    _id: string;
+    position: number;
+    title: string;
+    data: Array<{
+        x: number | string;
+        y: number;
+    }>;
+    xAxisLabel: string;
+    yAxisLabel: string;
+}
+
+interface TableData {
+    type: {
+        name: string,
+        subtype: string
+    },
+    _id: string,
+    position: number,
+    title: string,
+    data: {
+        id: number,
+        name: string,
+        age: number,
+        email: string
+    }[],
+    columns: {
+        key: string,
+        header: string
+    }[]
+}
+
+interface MultiPieGraphData {
+    type: {
+        name: 'pie';
+        subtype: 'multi-graph';
+    };
+    _id: string;
+    position: number;
+    title: string;
+    data: PieGraph[];
+}
+
+interface Widget {
+    [key: string]: Card | List | PieGraph | LineGraph | BarGraph | TableData | MultiPieGraphData
+}
+
+
+export { Dashboard, DashboardData, Widget } 
