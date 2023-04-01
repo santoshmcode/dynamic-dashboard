ba0b0e9b79c73fc345a009c96a43af5a3467c7a5
Santosh Mane <santoshmane619@gmail.com>
2023-04-01 19:20:55 +0530

    server: added wedget route

--- server/src/routers/dashboard.router.ts
+++ server/src/routers/dashboard.router.ts
@@ -15,17 +15,34 @@ router.get('/', (req: Request, res: Response) => {
     });
 });
 
+let count = 0
 router.get('/:id', (req: Request, res: Response) => {
-    console.log("Inside GET /:id")
+    console.log("Inside GET /:id", count++)
     fs.readFile(__dirname + '/../db/dashboard.json', 'utf8', (err, data: any) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
         }
-        const id: any = req.params.id
+        const id: string = req.params.id
         const jsonData = JSON.parse(data);
         const singleDashBoardData = jsonData[id]
         return res.send(singleDashBoardData);
+    });
+});
+
+router.get('/:id/widget/:id2', (req: Request, res: Response) => {
+    console.log("Inside GET widget /:id",)
+    fs.readFile(__dirname + '/../db/dashboard.json', 'utf8', (err, data: any) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Internal Server Error');
+        }
+        console.log("req.params",)
+        const { id, id2 } = req.params
+        console.log('id2:', id2, 'id:', id)
+        const jsonData = JSON.parse(data);
+        const singleDashBoardData = jsonData[id].filter((el: any) => el._id === id2)
+        return res.send(singleDashBoardData[0]);
     });
 });
 
