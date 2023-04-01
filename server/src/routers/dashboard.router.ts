927c8a8200f6c400dfd8627708c28e375513778b
Santosh Mane <santoshmane619@gmail.com>
2023-04-01 16:25:35 +0530

    server: fixed file path issue

--- server/src/routers/dashboard.router.ts
+++ server/src/routers/dashboard.router.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    fs.readFile('db/dashbord-one.json', 'utf8', (err, data) => {
+    fs.readFile(__dirname + '/../db/dashboard.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
