621fb65eda75f8e35a31abeef1f7bfdc6e1f9151
Santosh Mane <santoshmane619@gmail.com>
2023-04-03 11:24:06 +0530

    client : added navbar animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Routers from "./routers";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./App.css";
 import { getAllDashboards } from "./apis";
 import { DashboardsInterface } from "./types/types";
@@ -9,6 +9,10 @@ function App() {
     const [allDashboardsData, setAllDashboardsData] = useState<
         DashboardsInterface[]
     >([]);
+    const [activeLink, setActiveLink] = useState("");
+
+    const location = useLocation();
+
     useEffect(() => {
         const fetchDashboards = async () => {
             const data = await getAllDashboards();
@@ -16,11 +20,20 @@ function App() {
         };
         fetchDashboards();
     }, []);
+
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location]);
+
     return (
         <div className="App">
             <nav className="navbar">
                 {allDashboardsData.map(({ route, lable, _id }) => (
-                    <Link key={_id} to={route}>
+                    <Link
+                        key={_id}
+                        to={route}
+                        className={route === activeLink ? "active" : ""}
+                    >
                         {lable}
                     </Link>
                 ))}
