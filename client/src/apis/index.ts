53f9fa059273b12f64c256a348f673c1475a7a4a
Santosh Mane <santoshmane619@gmail.com>
2023-04-04 16:17:50 +0530

    deployment

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL = "https://mysterious-zipper-tuna.cyclic.app/api";
 
 const getAllDashboards = async () => {
     try {
