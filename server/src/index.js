d47efa4f95dab28e36106330be3f6da8bffa374f
Santosh Mane <santoshmane619@gmail.com>
2023-04-04 16:02:35 +0530

    deployment

--- server/src/index.js
+++ server/src/index.js
@@ -13,6 +13,4 @@ app.get("/", (req, res) => {
     res.send("Get");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT || 3000);
